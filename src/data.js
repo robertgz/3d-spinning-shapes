bbd3fcd5a32deacf6df4691e2b27e483e86ff54d
Robert <robertgz@users.noreply.github.com>
2018-11-20 21:31:12 -0800

    work in progress

---
+++ src/data.js
@@ -0,0 +1,111 @@
+var sceneData = {
+  scene: null,
+  renderer: null,
+  camera: null,
+  spotLight: null,
+  ambientLight: null,
+  myMesh: null,
+  stats: null,
+  step: 0,
+  clock: null,
+};
+
+var sceneObjects = { // sceneObjects.commonBoxData.
+  commonBoxData: {
+    sideLength: 1.25,
+    rotAcceleration: 0.01,
+    rotMin: 0.05,
+    rotMax: 0.2,
+    dampingFactor: 0.05,
+    dampingFactor2: 0.05 * 0.75,
+    dampingThreshold: 0.01,
+  },
+  templateBoxData: {
+    name: null,
+    sideLength: 1.25,
+    color: 0xffffff,
+    rotAngle: 0, // rotation angle that is used to set for the object
+    rotIncrement: 0, // amount added/subtracted to/from rotAngle to change the rotation
+    rotDirection: 0, // -1, 0, 1
+    applyDamping: false,
+    position: { x: 0, y: 0, z: 0 },
+  },
+  boxInits: [
+    { name: "box1", positionX: 0, 
+      keyboardKeys: [
+        { key: 'Q', direction: 1 },
+        { key: 'A', direction: -1 },
+      ]
+    },
+    { name: "box2", positionX: 2, 
+      keyboardKeys: [
+        { key: 'W', direction: 1 },
+        { key: 'S', direction: -1 },
+      ]
+    },
+    { name: "box3", positionX: 4, 
+      keyboardKeys: [
+        { key: 'E', direction: 1 },
+        { key: 'D', direction: -1 },
+      ]
+    },
+    { name: "box4", positionX: 6,
+      keyboardKeys: [
+        { key: 'R', direction: 1 },
+        { key: 'F', direction: -1 },
+      ]
+    },
+    { name: "box5", positionX: 8,
+      keyboardKeys: [
+        { key: 'T', direction: 1 },
+        { key: 'G', direction: -1 },
+      ]
+    },
+  ],
+  boxList: {}, // rename boxes later
+  keyEventLookup: {
+    // "letter": { objectName: "", direction: "" },
+  },
+};
+
+var gltfMeshData = {
+  meshes: [
+    {
+      name: "CubeQ_exp",
+      position: { x: 2, y: 0, z: 0 },
+      color:  0xffffff
+    },
+    {
+      name: "CubeW_exp",
+      position: { x: 5, y: 0, z: 0 },
+      color:  0xffffff
+    },
+    {
+      name: "CubeE_exp",
+      position: { x: 8, y: 0, z: 0 },
+      color:  0xffffff
+    },
+    {
+      name: "CubeR_exp",
+      position: { x: 11, y: 0, z: 0 },
+      color:  0xffffff
+    },
+    {
+      name: "CubeT_exp",
+      position: { x: 14, y: 0, z: 0 },
+      color:  0xffffff
+    }
+  ]
+};
+
+var StoreData = {
+  message: 'dat2.gui',
+  speed: 0.8,
+  rotationZmultiplier: 1,
+  rotationYmultiplier: 1,
+  cubeColor: 0xffffff * Math.random(),
+  clearColor: 0xeeeeee,
+  background: 0xeeeeee,
+  ambientIntensity: 1.0,
+  // this.cubeColor: "#00ff00";
+};
---
+++ src/index.js
@@ -0,0 +1,291 @@
+function init() {
+  // console.log("Using Three.js version: " + THREE.REVISION);
+
+  let stats = initStats();
+  document.body.appendChild(stats);  
+
+  let datGUI = createDAT();
+  setupScene();
+
+  sceneData.myMesh = createMesh();
+  sceneData.scene.add( sceneData.myMesh );
+  loadFile();
+
+  sceneData.clock = new THREE.Clock();
+  
+  // sceneData.scene.add( createBoxes() );
+
+  // add the output of the renderer to the html page
+  document.body.appendChild(sceneData.renderer.domElement);
+
+  keyBoardEvents();
+
+  animate();
+}
+
+function keyBoardEvents() {
+  function updateSpin(event) { // move this outside of keyBoardEvents 
+    // console.log('Key pressed ' + event.key)
+
+    function evalRot(shape, sign) { // sign = 1 or -1
+      shape.applyDamping = false;
+
+      var commonData = sceneObjects.commonBoxData;
+      var rotIncrement = shape.rotIncrement;
+      rotIncrement += commonData.rotAcceleration * sign;
+
+      if (Math.abs(rotIncrement) > commonData.rotMax) {
+        // limit the rotation speed to rotMax
+        rotIncrement = commonData.rotMax * sign;
+        console.log('Max rotation ' + sign);
+
+      } else if (Math.abs(rotIncrement) < commonData.rotMin) { 
+        // add a minimum rotation speed
+        rotIncrement += commonData.rotMin * sign;
+      }
+
+      shape.rotIncrement = rotIncrement;
+    }
+
+    var name = sceneObjects.keyEventLookup[event.key.toUpperCase()];
+    if (name) {
+      evalRot(sceneObjects.boxList[name.objectName], name.direction);
+    }
+  }
+
+  function enableDamping(event) {    
+    var name = sceneObjects.keyEventLookup[event.key.toUpperCase()];
+    if (name) {
+      sceneObjects.boxList[name.objectName].applyDamping = true;
+    }    
+    sceneData.clock.getDelta();
+
+    return;
+  }
+
+  window.addEventListener("keydown", updateSpin);
+  window.addEventListener("keyup", enableDamping);
+}
+
+function setupScene() {
+  sceneData.scene = new THREE.Scene();
+
+  sceneData.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  sceneData.camera.position.z = 10;
+  sceneData.camera.position.x = 4;
+
+  sceneData.renderer = new THREE.WebGLRenderer();
+  sceneData.renderer.setClearColor( StoreData.background );
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+  sceneData.renderer.setSize(500, 400);
+
+  // add lights
+  sceneData.spotLight = new THREE.SpotLight( 0xffffff );
+  sceneData.spotLight.position.set( -40, 60, -10 );
+  sceneData.scene.add( sceneData.spotLight );
+  
+  sceneData.ambientLight = new THREE.AmbientLight( 0x0c0c0c, sceneData.ambientIntensity );
+  sceneData.scene.add( sceneData.ambientLight ); 
+
+  // show axes in the screen
+  sceneData.scene.add(new THREE.AxesHelper(5));
+  
+  return sceneData.scene;
+}
+
+// function animate() {
+var animate = function () {
+  sceneData.stats.update();
+  requestAnimationFrame( animate );
+  
+  var datData = StoreData;
+  var object = sceneData.myMesh;
+  object.rotation.z += 0.01 * datData.rotationZmultiplier;
+  object.rotation.y += 0.01 * datData.rotationYmultiplier;
+
+  sceneData.step += 0.04;
+  object.position.x = 0 + ( 1 * ( Math.cos(sceneData.step) ) );  
+
+  applyRotationToList(sceneObjects.boxList);
+  
+  sceneData.renderer.render(sceneData.scene, sceneData.camera);
+}
+
+function applyRotationToList(objectList) {
+  var time = sceneData.clock.getDelta();
+  for (var item in objectList) {
+    // console.log(objectList[item]);
+    applyRotation( objectList[item] );
+
+    if (objectList[item].applyDamping) {
+      applyDamping(objectList[item], time);
+    }    
+  }
+}
+
+function applyRotation(object) {
+  // Should a reference to the scene object be stored in the sceneData list of objects?
+
+  var sceneObj = sceneData.scene.getObjectByName(object.name);  
+  object.rotAngle += object.rotIncrement;
+  object.rotDirection = Math.sign(object.rotIncrement); // fix/check !
+  sceneObj.rotation.x = object.rotAngle;
+}
+
+function applyDamping(object, time) {
+  var rotSpeed = Math.abs(object.rotIncrement);
+  // var dampingFactor
+
+  if (rotSpeed < sceneObjects.commonBoxData.dampingFactor) {
+    rotSpeed -= time * sceneObjects.commonBoxData.dampingFactor2;
+    object.rotIncrement = rotSpeed * object.rotDirection;
+
+    if (rotSpeed < sceneObjects.commonBoxData.dampingThreshold) {      
+      object.applyDamping = false;
+      object.rotIncrement = 0;
+    }
+  } else { // if (rotSpeed > 0)
+    rotSpeed -= time * sceneObjects.commonBoxData.dampingFactor;
+    object.rotIncrement = rotSpeed * object.rotDirection;
+  }
+  object.rotDirection = Math.sign(object.rotIncrement);
+}
+
+function createMesh() {
+  var sideLength = 1;
+  var datData = StoreData;
+  var geometry = new THREE.BoxGeometry( sideLength, sideLength, sideLength);
+  // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  // var material = new THREE.MeshLambertMaterial( { color: datData.cubeColor } );
+  var material = new THREE.MeshPhongMaterial( { color: datData.cubeColor } );
+  var cube = new THREE.Mesh( geometry, material);
+  cube.name = "moving cube";
+  
+  return cube;
+}
+
+function createBoxes() {
+  var group = new THREE.Group();
+
+  console.log('Box names');
+  var boxTemplateJSON = JSON.stringify(sceneObjects.templateBoxData);
+
+  sceneObjects.boxInits.forEach(function(item) {
+    sceneObjects.boxList[item.name] = JSON.parse(boxTemplateJSON);  
+    sceneObjects.boxList[item.name].name = item.name;
+    sceneObjects.boxList[item.name].color = 0xffffff * Math.random();
+    sceneObjects.boxList[item.name].position.x = item.positionX;
+
+    // register keys to be looked up when keys are pressed
+    if (item.keyboardKeys) {
+      item.keyboardKeys.forEach(function (keyData){
+        sceneObjects.keyEventLookup[keyData.key] = { objectName: item.name, direction: keyData.direction };
+      });
+      // console.log(sceneObjects.keyEventLookup);
+    }
+  });
+  
+  for (var item in sceneObjects.boxList) {
+    group.add( createBox( sceneObjects.boxList[item] ) );
+  }
+
+  return group;
+}
+
+function createBoxData() {
+
+}
+
+function createBox(boxData, positionX) { // creates THREEjs mesh
+  var sideLength = sceneObjects.commonBoxData.sideLength;
+  var geometry = new THREE.BoxGeometry( sideLength, sideLength, sideLength );
+  var material = new THREE.MeshPhongMaterial( { color: boxData.color } );
+  var box = new THREE.Mesh( geometry, material );
+  box.name = boxData.name;
+  box.position.x = boxData.position.x;
+
+  return box;
+}
+
+function loadFile() {
+  var loader = new THREE.GLTFLoader();
+
+  // loader.load( './assets/Box.gltf', function ( gltfData ) {
+  loader.load( './assets/QWERT_Cubes.gltf', function ( gltfData ) {
+    console.log('QWERT_Cubes.gltf loaded');
+    // console.log(gltfData);
+    // console.log(gltfData.scene);
+
+    // let scale = 25.0;
+    // let scale = 1.0;
+    // gltfData.scene.scale.x = scale;
+    // gltfData.scene.scale.y = scale;
+    // gltfData.scene.scale.y = scale;
+    // sceneData.scene.add( gltfData.scene );
+
+    // console.log('sceneData.scene');
+    // console.log(sceneData.scene);
+       
+    var meshes = buildMeshes(sceneData.scene, gltfData.scene);
+    sceneData.scene.add(meshes);
+  
+  }, undefined, function ( error ) {  
+    console.error( error );  
+  } );
+}
+
+function buildMeshes(existingScene, importedScene) {
+  var meshGroup = new THREE.Group();
+  var phongMaterial = new THREE.MeshPhongMaterial( { color: 0x550000, emissive: 0x000055 } );
+  //, specular: 0xffffff, shininess: 50
+
+  gltfMeshData.meshes.forEach(function (item) {
+    // console.log(item.name);
+    var mesh = importedScene.getObjectByName(item.name);
+    mesh.position.x = item.position.x;
+    mesh.position.y = item.position.y;
+    mesh.position.z = item.position.z;
+    mesh.material = phongMaterial;
+    // console.log(mesh);
+    meshGroup.add(mesh);
+  });
+
+  return meshGroup;
+}
+
+function createDAT() {
+  var datData = StoreData;
+  var gui = new dat.GUI();
+  gui.add(datData, 'rotationZmultiplier', -25, 25).step(1);
+  gui.add(datData, 'rotationYmultiplier', -25, 25, 1);
+  gui.add(datData, 'ambientIntensity', -1, 50, 1).onChange(function (e) {
+      sceneData.ambientLight.intensity = e ;
+  });
+  gui.addColor(datData, 'cubeColor').onChange(function (e) {
+    if (sceneData.myMesh !== null) {
+      sceneData.myMesh.material.color.setHex( e );
+    }    
+  });
+  gui.addColor(datData, 'background').onChange(function (e) {
+      sceneData.renderer.setClearColor( e );
+  });
+
+  return gui;
+}
+
+function initStats() {
+  var stats = sceneData.stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+
+  return stats.domElement;
+} 
+
+// issues
+/*
+  when browser is restored after minamizing a stopped cube is spinning
+  the boxes slow down at different rates, this may be due to them using the same clock
+
+*/
